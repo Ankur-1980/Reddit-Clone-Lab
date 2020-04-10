111e6f6c4fcaa52ffaaac0910ed18f1aae7679d8
Ankur <vasavada.ankur@gmail.com>
2020-04-10 11:16:10 -0400

    Init main component & service files

--- reddit-clone/src/app/app.module.ts
+++ reddit-clone/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RedditComponent } from './components/reddit/reddit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedditComponent
   ],
   imports: [
     BrowserModule
