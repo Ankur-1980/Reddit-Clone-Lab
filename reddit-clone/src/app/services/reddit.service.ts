1466f641647e552359f768f11ce95d1375066bc7
Ankur <vasavada.ankur@gmail.com>
2020-04-10 14:26:07 -0400

    New SubReddit Input Works

--- reddit-clone/src/app/services/reddit.service.ts
+++ reddit-clone/src/app/services/reddit.service.ts
@@ -11,6 +11,13 @@ export class RedditService {
   constructor(private httpClient: HttpClient) {}
 
   getData() {
+    console.log(`getDataService ${this.url}`);
     return this.httpClient.get(`${this.url}`);
+  }
+
+  changeSubReddit(newSubReddit) {
+    this.subReddit = newSubReddit;
+    this.url = `https://www.reddit.com/r/${this.subReddit}/.json`;
+    this.getData();
   }
 }
