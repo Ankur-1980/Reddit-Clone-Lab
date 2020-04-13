65554c6551221ab70021ef2041c5bc67a5dc4748
Ankur <vasavada.ankur@gmail.com>
2020-04-13 14:38:32 -0400

    fixed 404 error
    image displays reddit thumbnail if no image is present

--- reddit-clone/src/app/components/posts/posts.component.ts
+++ reddit-clone/src/app/components/posts/posts.component.ts
@@ -6,9 +6,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
+  show: boolean = true;
+  noPic =
+    'https://external-preview.redd.it/iDdntscPf-nfWKqzHRGFmhVxZm4hZgaKe5oyFws-yzA.png?auto=webp&s=38648ef0dc2c3fce76d5e1d8639234d8da0152b2';
+
   @Input() post;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.checkImage();
+  }
+
+  checkImage() {
+    if (
+      this.post.data.thumbnail === 'self' ||
+      this.post.data.thumbnail === ''
+    ) {
+      this.post.data.thumbnail = this.noPic;
+    }
+  }
 }
